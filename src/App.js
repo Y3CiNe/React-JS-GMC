25bc0a7e3a6e071fae07558118ccb3f6cc9aa496
Y3CiNe <59936894+Y3CiNe@users.noreply.github.com>
2021-11-24 13:34:15 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import Nav from "./components/navbar";
+import Form from "./components/form";
+import Footer from "./components/footer";
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Form />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./footer.css";
+export default function footer() {
+  return (
+    <div className="footer">
+      <p>form page</p>
+    </div>
+  );
+}
